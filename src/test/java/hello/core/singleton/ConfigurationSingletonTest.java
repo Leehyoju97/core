f1efa9cac0e87843c740c4eb2d5e57e628ab3215
Leehyoju97 <dlgywn2420@gmail.com>
2023-03-20 00:02:22 +0900

    feat : component scan, autowired 이용해 bean 등록

---
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,28 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.OrderServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        System.out.println("memberService = " + memberService);
+        System.out.println("orderService = " + orderService);
+        System.out.println("memberRepository = " + memberRepository);
+
+//        assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+//        assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+}
